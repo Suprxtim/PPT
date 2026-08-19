a52cc24d76441a49291a4f17f765df01890cb457
Supratim Haldar <supratimhaldar05@gmail.com>
2026-08-19 23:55:17 +0530

    74. Search a 2D Matrix

---
+++ DSA/Binary Search/search2dmatrix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        m=len(matrix)   #row
+        n=len(matrix[0]) #column
+        low=0
+        high=(m*n)-1
+
+        while low<= high:
+            mid=(low+high)//2
+
+            row=mid//n
+            column=mid% n  
+
+            mid_element=matrix[row][column]
+
+            if mid_element == target:
+                return True
+            elif mid_element< target:
+                low=mid+1
+            else:
+                high=mid-1
+        return False
